7cccec7f29affc8ffb0e5506b8c90f1c47e0ad84
Slobodan Milivojevic <seltik86@hotmail.com>
2019-10-27 23:07:20 +0100

    hooks added

--- movies-eternal/src/movies/movie-searched/MovieSearched.js
+++ movies-eternal/src/movies/movie-searched/MovieSearched.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faList, faStar } from '@fortawesome/free-solid-svg-icons';
 import './MovieSearched.css';
 import PropTypes from 'prop-types';
 
-class MovieSearched extends Component {
-    Favorites = fav => {
-        this.props.favorites(fav);
+const MovieSearched = ({ movie, favorites, watch, inwatchlist, infavorites }) => {
+    const Favorites = fav => {
+        favorites(fav);
     }
 
-    render() {
-        return (
-            <div className="movie-wrapper">
-                <div className="movie-title">{this.props.movie.title}</div>
-                {this.props.movie.poster_path === null ? (
-                    <img src="https://via.placeholder.com/185x278.png" alt="No poster" />
-                ) : (
-                    <img src={"https://image.tmdb.org/t/p/w185_and_h278_bestv2/" + this.props.movie.poster_path} alt={this.props.movie.title} className="movie-poster" />
-                )}
-                <div className="movie-info">
-                    <div className="movie-add-watch"><FontAwesomeIcon icon={faList} /></div>
-                    <div className={"movie-add-favorite " + (this.props.infavorites ? 'active' : 'disabled')} onClick={() => {this.Favorites(this.props.movie.id)}}><FontAwesomeIcon icon={faStar} /></div>
-                    <div className="movie-popularity">Popularity: {this.props.movie.popularity}</div>
-                    <div className="movie-release">Release date: {this.props.movie.release_date}</div>
-                </div>
-            </div>
-        );
+    const WatchLater = movie => {
+        watch(movie);
     }
+
+    return (
+        <div className="movie-wrapper">
+            <div className="movie-title">{movie.title}</div>
+            {movie.poster_path === null ? (
+                <img src="https://via.placeholder.com/185x278.png" alt="No poster" />
+            ) : (
+                <img src={"https://image.tmdb.org/t/p/w185_and_h278_bestv2/" + movie.poster_path} alt={movie.title} className="movie-poster" />
+            )}
+            <div className="movie-info">
+                <div className={"movie-add-watch " + (inwatchlist ? 'active' : 'disabled')} onClick={() => {WatchLater(movie.title)}}><FontAwesomeIcon icon={faList} /></div>
+                <div className={"movie-add-favorite " + (infavorites ? 'active' : 'disabled')} onClick={() => {Favorites(movie.id)}}><FontAwesomeIcon icon={faStar} /></div>
+                <div className="movie-popularity">Popularity: {movie.popularity}</div>
+                <div className="movie-release">Release date: {movie.release_date}</div>
+            </div>
+        </div>
+    );
 }
 
 MovieSearched.propTypes = {
--- movies-eternal/src/movies/movies/Movies.js
+++ movies-eternal/src/movies/movies/Movies.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 import MovieSearched from '../movie-searched/MovieSearched';
 import Spinner from '../../assets/Spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList } from '@fortawesome/free-solid-svg-icons';
 import './Movies.css';
 import PropTypes from 'prop-types';
 
 class Movies extends Component {
     state = {
-        favorites: []
+        favorites: [],
+        watch: [],
+        buttonList: false
     };
 
     componentDidMount() {
-        const local = JSON.parse(localStorage.getItem("favorites"));
-        if(local !== null) {
+        const localFavorites = JSON.parse(localStorage.getItem("favorites"));
+        const localWatch = JSON.parse(localStorage.getItem("watchlist"));
+
+        if(localFavorites !== null) {
             this.setState({
-                favorites: local
+                favorites: localFavorites
+            });
+        }
+        if(localWatch !== null) {
+            this.setState({
+                watch: localWatch
             });
         }
     }
@@ -37,15 +48,52 @@ class Movies extends Component {
         }
     }
 
+    WatchList = watchMovie => {
+        let watchUpdate = this.state.watch;
+
+        if(watchUpdate.includes(watchMovie)) {
+            watchUpdate = watchUpdate.filter(item => item !== watchMovie);
+            localStorage.setItem("watchlist", JSON.stringify(watchUpdate));
+            this.setState({
+                watch: watchUpdate
+            });
+        }
+        else {
+            watchUpdate.push(watchMovie);
+            localStorage.setItem("watchlist", JSON.stringify(watchUpdate));
+            this.setState({
+                watch: watchUpdate
+            });
+        }
+    }
+
+    ButtonListToggle = () => {
+        this.setState({
+            buttonList: !this.state.buttonList
+        });
+    }
+
     render() {
         if(this.props.loading) {
             return <Spinner />
         } else {
             return (
-                <div className="grid-movies">
-                    {this.props.movies.map(movie => (
-                        <MovieSearched key={movie.id} movie={movie} infavorites={this.state.favorites.includes(movie.id)} favorites={this.FavoriteList} />
-                    ))}
+                <div className="grid">
+                    {this.state.watch.length !== 0 &&
+                        <div className="watch-list-holder">
+                            <div className={"watch-button " + (this.state.buttonList ? 'active' : 'disabled')} onClick={() => {this.ButtonListToggle()}}><FontAwesomeIcon icon={faList} /></div>
+                            <ul className="watch-list">
+                                {this.state.watch.map(moviename => {
+                                    return <li key={moviename}>{moviename}</li>;
+                                })}
+                            </ul>
+                        </div>
+                    }
+                    <div className="grid-movies">
+                        {this.props.movies.map(movie => (
+                            <MovieSearched key={movie.id} movie={movie} inwatchlist={this.state.watch.includes(movie.title)} infavorites={this.state.favorites.includes(movie.id)} watch={this.WatchList} favorites={this.FavoriteList} />
+                        ))}
+                    </div>
                 </div>
             );
         }
